58ef43bb8a2f19b9a1112fa64230e809466cf7b8
VISHWADEEP DIPAKRAO KIRTAKAR <106275197+vishwakirtakar@users.noreply.github.com>
2023-10-03 15:44:56 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Navi from "./compo/Navi";
+import Shoe from "./compo/shoe";
+
+
+const App =  () => {
+ return (
+ <>
+    <Navi/>
+    <Shoe/>
+
+ </>
+ );
+};
+export default App;
---
+++ Navi.jsx
@@ -0,0 +1,19 @@
+const navi = ()=>{
+
+    return (
+    <nav className="container">
+    <div className="logo">
+     <img src="/images/brand_logo (1).png" alt="Logo"/>
+     </div>
+<ul className="bar">
+   <li href="#">Manu</li>
+   <li href="#">Location</li>
+   <li href="#">About</li>
+   <li href="#">Contact</li>
+</ul>
+
+  <button>login</button>
+</nav>  
+    );
+};
+export default navi;    
